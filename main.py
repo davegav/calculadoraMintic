8f979dd920bdc3510d70cae8615d89edb5fee97e
AndradeW <wilson00749@gmail.com>
2020-11-28 16:57:17 -0500

    Ya hice la suma

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 #Aqui va el codigo de la calculadora
 print("Hola mundo")
+
+
+#----- Calculadora------
+
+x = input(Ingrese un numero: )
+y = input(Ingrese otro numero: )
+
+#   SUMA
+def suma(x, y):
+    return x+y
+
+
